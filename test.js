9b9718dd092412a77453e5820a0e3596645529c2
AkieNakai <ayalake156@gmail.com>
2020-05-06 17:02:14 +0900

    doneとdoneリストのテスト

--- test.js
+++ test.js
@@ -7,4 +7,8 @@ todo.todo('クリーニング屋に服を取りに行く');
 todo.todo('ディナーのお店を調べる');
 assert.deepEqual(todo.list(), ['クリーニング屋に服を取りに行く', 'ディナーのお店を調べる']);
 
+// doneとdonelistのテスト
+todo.done('クリーニング屋に服を取りに行く');
+console.log(todo.donelist());
+
 console.log('テストは正常に完了しました');
